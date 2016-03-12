76384928ac6f940907c14b1813197d660604c6c1
Jonas Büchel <Jonas Bchel>
2016-03-12 18:02:40 +0100

    added a function to send the elevator which has the smallest load to the requested level

--- my-solution.js.js
+++ my-solution.js.js
@@ -1,40 +1,72 @@
 {
     init: function(elevators, floors) {
-        
-       var elevatorRegisterIdle = function(elevator){
-           elevator.on("idle", function() {
-               elevator.goToFloor(Math.ceil(floors.lenght/2));
+
+        var getElevatorWithSmallestLoad = function(){
+
+            var smallestLoad = {
+                elevatorId: 0,
+                load: 1
+            };
+
+            for(var i = 0; i < elevators.length; i++){
+                var currentElevator = elevators[i];
+                var currentElevatorLoadFactor = currentElevator.loadFactor();
+
+                if(currentElevatorLoadFactor < smallestLoad.load){
+                    smallestLoad.elevatorId = i;
+                    smallestLoad.load = currentElevatorLoadFactor;
+                }
+            }
+
+            return smallestLoad.elevatorId;
+
+        };
+
+        // Whenever the elevator is idle (has no more queued destinations) ...
+        var elevatorRegisterIdle = function(elevator){
+            elevator.on("idle", function() {
+                elevator.goToFloor(Math.floor(floors.length/2));
             });
-       };
-       
-        
+        };
+
+        var elevatorRegisterButtons = function(elevator){
+            elevator.on("floor_button_pressed", function(floorNum) { 
+                elevator.goToFloor(floorNum);
+            } );
+
+        };
+
+        var  floorRegisterButtons = function(floor){
+            floor.on("up_button_pressed down_button_pressed", function(){
+
+                //find elevator with smallest load
+                var elevatorToSendId    = getElevatorWithSmallestLoad();
+                var elevatorToSend      = elevators[elevatorToSendId];
+
+                //send elevator to requested floor
+                elevatorToSend.goToFloor(this.level);
+
+            });
+        };
+
+
         //AWAKE ELEVATORS
         for(var elelvatorCounter = 0; elelvatorCounter < elevators.length ; elelvatorCounter ++){
             var currentElevator = elevators[elelvatorCounter];
 
-            console.log(elelvatorCounter);
+            elevatorRegisterIdle(currentElevator);
+            elevatorRegisterButtons(currentElevator);
 
         }
 
-        
-        // Whenever the elevator is idle (has no more queued destinations) ...
-        
-        
-        /*elevator1.on("floor_button_pressed", function(floorNum) { 
-            elevator1.goToFloor(floorNum);
-        } );
-        
+        //AWAKE FLOOR BUTTONS
         for(var i = 0 ; i < floors.length ; i++){
             var currentFloor = floors[i];
-                        
-            currentFloor.on("up_button_pressed down_button_pressed", function(){
-                elevator1.goToFloor(this.level);
-            });
-      
-        }*/
-                
+            floorRegisterButtons(currentFloor);
+        }
+
     },
-    update: function(dt, elevators, floors) {
-        // We normally don't need to do anything here
-    }
+        update: function(dt, elevators, floors) {
+            // We normally don't need to do anything here
+        }
 }
